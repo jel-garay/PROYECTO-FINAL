20abd3280f1382f8830021c86d77d5d1ecc4ebcf
jel <jelsabingarayelexpuru@hotmail.com>
2023-03-13 18:25:25 +0100

    jel

--- main.py
+++
@@ -1,9 +0,0 @@
-
-
-
-import streamlit as st
-import streamlit.components.v1 as components
-#components.iframe("https://docs.streamlit.io/en/latest")
-components.iframe("mapa.py")
-
-streamlit run main.py
